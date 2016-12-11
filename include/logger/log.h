979b1ab49b5ec994e2d803eb960f7a11bc3f9494
nefthon <fjgo1986@gmail.com>
2016-12-11 20:45:45 +0100

    Añadidos flags al Logger

--- include/logger/log.h
+++ include/logger/log.h
@@ -17,6 +17,13 @@
 #define LogColorMethodDefault 14    // Color para el texto que muestra el nombre del método desde el que se crea el Log.
 
 /*
+    Flags para los logs.
+*/
+#define LOGFLAG_DETAILED        0x01    // Envía información detallada sobre el fichero que envía el Log.
+#define LOGFLAG_PRINTCONSOLE    0x02    // Envía el log a la consola.
+//#define LOGFLAG_PRINTFILE       0x04    // Envía el log a un archivo físico.
+
+/*
 * @brief Logs en consola
 * Clase encargada de enviar los Log a pantalla (TODO: a un archivo en un futuro):
 * Macros de información detallada (Envían __FILE__, __LINE__ y __func__ al texto a mostrar):
@@ -74,6 +81,7 @@ private:
     LogLevel _logLevel;     // Guarda el LogLevel o importancia del Log
 	std::string _txt;       // Guarda el texto que se pretende mostrar en el Log (Independientemente del encabezado que se muestre).
     bool _isDetailed;       // True = muestra los atributos de la sección 'Información detallada' entre _logLevel y _txt.
+    unsigned int _flags;    // Flags del Log.
 
     // Información detallada
     const char *_file,      // Guarda el nombre del archivo (y su ruta) desde el que se produce el Log.
@@ -82,7 +90,7 @@ private:
 
 
 public:
-    Log(LogLevel logLvl, const char* file, int line, const char* method, const char* txt) {
+    Log(LogLevel logLvl, const char* file, int line, const char* method, const char* txt, unsigned int flags = LOGFLAG_PRINTCONSOLE) {
         if (logLvl >= Log::LOGLVL_QTY) {
             _logLevel = LogLevel(LOGLVL_QTY - 1);   // Comprobando que el LogLevel no exceda a los definidos, se pondrá por defecto al último LogLevel existente (el de más alta importancia).
         }
@@ -92,10 +100,14 @@ public:
         _file = file;
         _line = line;
         _method = method;
-        _isDetailed = true;
         _txt = txt;
+        _flags = flags;
+        if (isFlag(LOGFLAG_DETAILED))
+            _isDetailed = true;
+        else
+            _isDetailed = false;
     }
-    Log(LogLevel logLvl, const char* txt) {
+    Log(LogLevel logLvl, const char* txt, unsigned int flags = LOGFLAG_PRINTCONSOLE) {
         if (logLvl >= Log::LOGLVL_QTY) {
             _logLevel = LogLevel(LOGLVL_QTY - 1);   // Comprobando que el LogLevel no exceda a los definidos, se pondrá por defecto al último LogLevel existente (el de más alta importancia).
         }
@@ -105,8 +117,9 @@ public:
         _file = "";
         _line = 0;
         _method = "";
-        _isDetailed = false;
         _txt = txt;
+        _flags = flags;
+        _isDetailed = false;
     }
     ~Log(){
     }
@@ -115,23 +128,29 @@ public:
     Imprime los datos del Log en la consola del programa.
     */
     void printText() {
-        HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(consola, getLevelColor());          // Asigna el Log Level Color
-        std::cout << getLevelString();                              // Imprime el Log Level Name
+        //if (isFlag(LOGFLAG_PRINTCONSOLE)) {
 
-        if (isDetailed()) {
-            SetConsoleTextAttribute(consola, LogColorFileDefault);  // Asigna el Log File Color a Default
-            std::cout << getFile() << ":";                          // Imprime separador.
+            HANDLE consola = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleTextAttribute(consola, getLevelColor());          // Asigna el Log Level Color
+            std::cout << getLevelString();                              // Imprime el Log Level Name
 
-            SetConsoleTextAttribute(consola, LogColorLineDefault);  // Asigna el Log Line Color a Default
-            std::cout << getLine() << ":";                          // Imprime separador.
+            if (isDetailed()) {
+                SetConsoleTextAttribute(consola, LogColorFileDefault);  // Asigna el Log File Color a Default
+                std::cout << getFile() << ":";                          // Imprime separador.
 
-            SetConsoleTextAttribute(consola, LogColorMethodDefault);// Asigna el Log Method Color a Default
-            std::cout << getMethod() << " = ";                      // Imprime separador.
-        }
+                SetConsoleTextAttribute(consola, LogColorLineDefault);  // Asigna el Log Line Color a Default
+                std::cout << getLine() << ":";                          // Imprime separador.
 
-        SetConsoleTextAttribute(consola, LogColorTextDefault);      // Asigna el Log Text Color a Default
-        std::cout << getText();
+                SetConsoleTextAttribute(consola, LogColorMethodDefault);// Asigna el Log Method Color a Default
+                std::cout << getMethod() << " = ";                      // Imprime separador.
+            }
+
+            SetConsoleTextAttribute(consola, LogColorTextDefault);      // Asigna el Log Text Color a Default
+            std::cout << getText() << std::endl;
+        //}
+        /*
+        TODO: if (isFlag(LOGFLAG_PRINTFILE))
+        */
     }
 
 private:
@@ -186,5 +205,13 @@ private:
     */
     const char* getMethod() {
         return _method;
+    }
+    /*
+    * @brief Comprueba si tiene el flag especificado.
+    * @param flag
+    * @return true si lo tiene, false si no.
+    */
+    bool isFlag(int flag) {
+        return (_flags & flag) ? true : false;
     }
 };
